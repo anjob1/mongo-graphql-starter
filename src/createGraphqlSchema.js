585b703ea4b6efd934275ce7a8173f630b533d50
anjob1 <46771744+anjob1@users.noreply.github.com>
2024-08-04 20:37:11 +1000

    Updated with efficient MongoDB operations and Atlas Search support

--- src/createGraphqlSchema.js
+++ src/createGraphqlSchema.js
@@ -2,12 +2,10 @@ import path from "path";
 import fs from "fs";
 import mkdirp from "mkdirp";
 
-import { MongoIdType, StringArrayType, MongoIdArrayType, IntArrayType, FloatArrayType } from "./dataTypes";
 import createTypeResolver from "./codeGen/createTypeResolver";
 import createGraphqlTypeSchema from "./codeGen/createTypeSchema";
 import createOutputTypeMetadata from "./codeGen/createTypeMetadata";
 import createMasterSchema from "./codeGen/createMasterSchema";
-
 import createMasterResolver from "./codeGen/createMasterResolver";
 import createTypeScriptTypes from "./codeGen/createTypeScriptTypes";
 import createTestSchema from "./codeGen/createTestSchema";
@@ -28,13 +26,13 @@ function createFile(path, contents, onlyIfAbsent, ...directoriesToCreate) {
 const formatGraphQL = code => prettier.format(code, { parser: "graphql" });
 const formatJs = code => prettier.format(code, { parser: "babel", printWidth: 120, arrowParens: "avoid", trailingComma: "none" });
 
-export default function (source, destPath, options = {}) {
+export default function createGraphqlSchema(source, destPath, options = {}, regenerateResolver = null) {
   return Promise.resolve(source).then(graphqlMetadata => {
     let rootDir = path.join(destPath, "graphQL");
     if (!fs.existsSync(rootDir)) {
       mkdirp.sync(rootDir);
     }
-    let typeLookup = new Map([]);
+    let typeLookup = new Map();
     Object.keys(graphqlMetadata).forEach(k => {
       typeLookup.set(k, graphqlMetadata[k]);
     });
@@ -43,8 +41,7 @@ export default function (source, destPath, options = {}) {
       type.__name = k;
       if (!type.fields) graphqlMetadata[k].fields = {};
       if (!type.fields._id && type.table) {
-        //add _id, and as a bonus, make it show up first in the list since the spec iterates object keys in order of insertion
-        type.fields = { _id: MongoIdType, ...type.fields };
+        type.fields = { _id: "MongoId", ...type.fields };
       }
       if (type.relationships) {
         Object.keys(type.relationships).forEach(k => {
@@ -55,7 +52,7 @@ export default function (source, destPath, options = {}) {
           let fkFieldId = type.fields[relationship.fkField];
           let keyField = relationship.keyField;
 
-          let fkArray = [StringArrayType, MongoIdArrayType, IntArrayType, FloatArrayType].includes(fkFieldId);
+          let fkArray = ["StringArray", "MongoIdArray", "IntArray", "FloatArray"].includes(fkFieldId);
           relationship.__isArray = (fkArray || keyField !== "_id") && !relationship.oneToOne;
           relationship.__isObject = !relationship.__isArray;
 
@@ -95,6 +92,10 @@ export default function (source, destPath, options = {}) {
     let types = [];
 
     modules.forEach(objectToCreate => {
+      if (regenerateResolver && regenerateResolver !== objectToCreate.__name) {
+        return;
+      }
+
       let objName = objectToCreate.__name;
       let modulePath = path.join(rootDir, objName);
       let objPath = path.join(modulePath, objName + ".js");
@@ -122,40 +123,43 @@ export default function (source, destPath, options = {}) {
 
     const schemaAdditions = (options.schemaAdditions || []).map(path => fs.readFileSync(path, { encoding: "utf8" }));
     const resolverAdditions = (options.resolverAdditions || []).map(uri => path.relative(rootDir, uri).replace(/\\/g, "/"));
-    fs.writeFileSync(
-      path.join(rootDir, "schema.js"),
-      formatJs(createMasterSchema(names, namesWithTables, namesWithoutTables, namesWriteable, schemaAdditions))
-    );
-
-    const schemaModule = require(path.join(rootDir, "schema.js"));
-    const masterSchema = formatGraphQL(schemaModule.default);
-    fs.writeFileSync(path.join(rootDir, "entireSchema.gql"), masterSchema);
-
-    try {
+    
+    if (!regenerateResolver) {
       fs.writeFileSync(
-        path.join(rootDir, "test-resolvers.js"),
-        formatJs(createTestSchema(names, namesWithTables, namesWithoutTables, namesWriteable, types))
+        path.join(rootDir, "schema.js"),
+        formatJs(createMasterSchema(names, namesWithTables, namesWithoutTables, namesWriteable, schemaAdditions))
       );
-    } catch (er) {
-      console.log("ERROR GENERATING RESOLVER TESTS", er);
+
+      const schemaModule = require(path.join(rootDir, "schema.js"));
+      const masterSchema = formatGraphQL(schemaModule.default);
+      fs.writeFileSync(path.join(rootDir, "entireSchema.gql"), masterSchema);
+
+      try {
+        fs.writeFileSync(
+          path.join(rootDir, "test-resolvers.js"),
+          formatJs(createTestSchema(names, namesWithTables, namesWithoutTables, namesWriteable, types))
+        );
+      } catch (er) {
+        console.log("ERROR GENERATING RESOLVER TESTS", er);
+      }
+
+      let result;
+
+      if (options.typings) {
+        result = createTypeScriptTypes(masterSchema, options.typings).catch(er => {
+          console.log("\n\nERROR GENERATING TS TYPES\n\n", er);
+        });
+      }
+
+      fs.writeFileSync(path.join(rootDir, "resolver.js"), formatJs(createMasterResolver(namesWithTables, namesWriteable, resolverAdditions)));
+      if (!options.hooks && !fs.existsSync(path.join(rootDir, "hooks.js"))) {
+        fs.writeFileSync(
+          path.join(rootDir, "hooks.js"),
+          formatJs(fs.readFileSync(path.resolve(__dirname, "./codeGen/processingHooksTemplate.js"), { encoding: "utf8" }))
+        );
+      }
+
+      return result;
     }
-
-    let result;
-
-    if (options.typings) {
-      result = createTypeScriptTypes(masterSchema, options.typings).catch(er => {
-        console.log("\n\nERROR GENERATING TS TYPES\n\n", er);
-      });
-    }
-
-    fs.writeFileSync(path.join(rootDir, "resolver.js"), formatJs(createMasterResolver(namesWithTables, namesWriteable, resolverAdditions)));
-    if (!options.hooks && !fs.existsSync(path.join(rootDir, "hooks.js"))) {
-      fs.writeFileSync(
-        path.join(rootDir, "hooks.js"),
-        formatJs(fs.readFileSync(path.resolve(__dirname, "./codeGen/processingHooksTemplate.js"), { encoding: "utf8" }))
-      );
-    }
-
-    return result;
   });
 }
